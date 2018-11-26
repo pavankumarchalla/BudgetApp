580a8b2db3a82273868d1d1031db997c2683cab1
Pavan Kumar C <pavan.challa23@gmail.com>
2018-11-26 14:28:28 +0530

    added methods to read the input data

--- app.js
+++ app.js
@@ -7,17 +7,44 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+
+        },
+
+        getDomStrings: function() {
+            return DOMStrings;
+        }
+
+
+    };
 
 })();
 
 
 var controller = (function(budgetCntrl, UICntrl) {
 
+    var Dom = UICntrl.getDomStrings();
+
     var cntrlAddItem = function() {
-        console.log('It works!!')
+        var input = UICntrl.getInput();
+        console.log(input)
     }
 
-    document.querySelector('.add__btn').addEventListener('click', cntrlAddItem);
+    document.querySelector(Dom.inputButton).addEventListener('click', cntrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
